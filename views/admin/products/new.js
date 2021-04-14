5884a23212e98139eb29fdb1075c1410ff50c7a4
PerishableQ <redemption.yep@gmail.com>
2021-04-14 20:00:23 +0300

    added image upload functionality

--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -7,9 +7,11 @@ module.exports = ({errors}) => {
         content: `
         <form method='POST' enctype='multipart/form-data'>
             <input placeholder='Title' name='title' />
+            ${getError(errors, 'title')}
             <input placeholder='Price' name='price' />
+            ${getError(errors, 'price')}
             <input type='file' name='image' />
-            <button type='submit'>
+            <button type='submit'>Submit</button>
         </form>
         `
     })
