87b6fb3836450fb4c6732b684b27331c4aa6a138
PerishableQ <redemption.yep@gmail.com>
2021-04-15 18:17:19 +0300

    debugged, MVP is done

--- views/carts/show.js
+++ views/carts/show.js
@@ -11,21 +11,22 @@ module.exports = ({items}) => {
         <div class="cart-item message">
         <h3 class="subtitle">${item.product.title}</h3>
         <div class="cart-right">
-          <div>
-            $${item.product.price}  X  ${item.quantity} = 
-          </div>
-          <div class="price is-size-4">
-            $${item.product.price * item.quantity}
-          </div>
-          <div class="remove">
-            <form method="POST">
-              <button class="button is-danger">                  
-                <span class="icon is-small">
-                  <i class="fas fa-times"></i>
-                </span>
-              </button>
-            </form>
-          </div>
+            <div>
+                $${item.product.price}  X  ${item.quantity} = 
+            </div>
+            <div class="price is-size-4">
+                $${item.product.price * item.quantity}
+            </div>
+            <div class="remove">
+                <form method="POST" action="/cart/products/delete">
+                <input type="hidden" value="${item.id}" name="itemId"/>
+                <button class="button is-danger">                  
+                    <span class="icon is-small">
+                    <i class="fas fa-times"></i>
+                    </span>
+                </button>
+                </form>
+            </div>
         </div>
       </div>
     `;
@@ -35,23 +36,23 @@ module.exports = ({items}) => {
 return layout({
   content: `
     <div id="cart" class="container">
-      <div class="columns">
-        <div class="column"></div>
-        <div class="column is-four-fifths">
-          <h3 class="subtitle"><b>Shopping Cart</b></h3>
-          <div>
-            ${renderedItems}
-          </div>
-          <div class="total message is-info">
-            <div class="message-header">
-              Total
+        <div class="columns">
+            <div class="column"></div>
+            <div class="column is-four-fifths">
+            <h3 class="subtitle"><b>Shopping Cart</b></h3>
+            <div>
+                ${renderedItems}
             </div>
-            <h1 class="title">$${totalPrice}</h1>
-            <button class="button is-primary">Buy</button>
-          </div>
+            <div class="total message is-info">
+                <div class="message-header">
+                Total
+                </div>
+                <h1 class="title">$${totalPrice}</h1>
+                <button class="button is-primary">Buy</button>
+            </div>
+            </div>
+            <div class="column"></div>
         </div>
-        <div class="column"></div>
-      </div>
     </div>
   `
     });
